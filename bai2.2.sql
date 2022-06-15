f0db5750173fa1b3a036f0b322f2f385968a538f
Kisibian108 <102302311+Kisibian108@users.noreply.github.com>
2022-06-15 16:31:42 +0700

    bai2.2

--- bai2.2.sql
+++ bai2.2.sql
@@ -1,3 +1,7 @@
+create database quan_ly_ban_hang;
+
+use quan_ly_ban_hang;
+
 create table customer(
 cID int primary key auto_increment,
 cName varchar(45),
@@ -6,12 +10,11 @@ cAge varchar(45)
 
 create table `order`(
 oID int primary key auto_increment,
+customer_id int ,
 oDate date,
 oTotalPrice int,
-customer_id int ,
 foreign key (customer_id) references customer(cID)
 );
-
 
 create table product(
 pID int primary key auto_increment,
@@ -20,9 +23,50 @@ pPrice int
 );
 
 create table order_detail(
-odQty int,
 orderID int ,
 productID int,
+odQty int,
 foreign key (orderID) references `order`(oID),
 foreign key (productID) references product(pID)
 );
+
+insert into customer(cname,cAge)
+values('Minh Quan','10'),('Ngoc Oanh', '20'),('Hong Ha', '50');
+
+select * from customer;
+select * from `order`;
+select * from order_detail;
+select * from product;
+
+insert into `order`
+values(1,1,'2006-03-21', Null),(2,2,'2006-03-23' ,Null),(3,1,'2006-03-16', Null);
+
+insert into product(pName,pPrice)
+values('May giat',3),('Tu lanh', 5),('Dieu hoa',7),('Quat',1),('Bep Dien',2);
+
+insert into order_detail
+values(1,1,3),(1,3,7),(1,4,2),(2,1,1),(3,1,8),(2,5,4),(2,3,3);
+
+select * from `order`;
+
+
+select * from customer
+left join `order` on customer.cID = `order`.customer_id
+where `order`.customer_id is null;
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+select `order`.oID,oDate, sum(pPrice * odQty) as gia_tien from `order` 
+ join order_detail on `order`.oID = order_detail.orderID
+ join product on order_detail.productID = product.pID
+group by order_detail.orderID;
+
+
+
+
+
+
+
+
+
+
+
